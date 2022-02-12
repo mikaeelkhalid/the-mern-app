dc1ab92eaf95afee87605de590eaead880520e65
Mikaeel Khalid <mikaeelkhalid@icloud.com>
2022-02-12 17:59:32 +0500

    adding description

--- backend/controllers/goalsController.js
+++ backend/controllers/goalsController.js
@@ -1,22 +1,34 @@
 module.exports = {
+  // @desc    Get all goals
+  // @route   GET /api/goals
+  // @access  Private
   getGoals: (req, res) => {
     res.status(200).json({
       message: 'Goals fetched successfully!',
     });
   },
 
+  // @desc    Create a goal
+  // @route   POST /api/goals
+  // @access  Private
   createGoal: (req, res) => {
     res.status(200).json({
       message: 'Goal created successfully!',
     });
   },
 
+  // @desc    Update a goal
+  // @route   PUT /api/goals/:id
+  // @access  Private
   updateGoal: (req, res) => {
     res.status(200).json({
       message: `${req.params.id} updated successfully!`,
     });
   },
 
+  // @desc    Delete a goal
+  // @route   DELETE /api/goals/:id
+  // @access  Private
   deleteGoal: (req, res) => {
     res.status(200).json({
       message: `${req.params.id} deleted successfully!`,
