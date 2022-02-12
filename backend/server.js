3875b5fd297701e8f8c6bd5b93dce4ce5850f54b
Mikaeel Khalid <mikaeelkhalid@icloud.com>
2022-02-12 18:04:54 +0500

    adding middleware

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,10 @@ const PORT = 5000;
 
 const app = express();
 
+app.use(express.json());
+
+app.use(express.urlencoded({ extended: false }));
+
 app.use('/api/goals', require('./routes/goalRoutes'));
 
 app.listen(PORT, () => {
