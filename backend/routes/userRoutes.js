7d1c1892e05b93d1f27d8688b41ae9ee5e4b8baf
Mikaeel Khalid <mikaeelkhalid@icloud.com>
2022-02-13 20:32:12 +0500

    protect user route

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-
+const { protect } = require('../middleware/authMiddleware');
 const {
   registerUser,
   loginUser,
@@ -9,6 +9,6 @@ const {
 
 router.post('/', registerUser);
 router.post('/login', loginUser);
-router.get('/me', getMe);
+router.get('/me', protect, getMe);
 
 module.exports = router;
